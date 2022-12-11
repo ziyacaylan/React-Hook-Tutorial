2ee58c6681c6b41e578efa9bd156c0bc257438e3
Ziya ÇAYLAN <ziyacaylan@gmail.com>
2022-12-11 19:26:46 +0300

    context api completed

--- src/tutorial/6-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/6-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,44 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import { data } from "../../../data";
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  const kaldirilmisKisi = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+  return (
+    <section>
+      <h3>Prop Drilling</h3>
+      <List people={people} kaldirilmisKisi={kaldirilmisKisi} />
+    </section>
+  );
 };
 
+const List = ({ people, kaldirilmisKisi }) => {
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+            kaldirilmisKisi={kaldirilmisKisi}
+          />
+        );
+      })}
+    </>
+  );
+};
+const SinglePerson = ({ id, name, kaldirilmisKisi }) => {
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button onClick={() => kaldirilmisKisi(id)}>Kaldır</button>
+    </div>
+  );
+};
 export default PropDrilling;
--- src/tutorial/7-useContext/setup/1-context-api.js
+++ src/tutorial/7-useContext/setup/1-context-api.js
@@ -1,46 +1,44 @@
-import React, { useState, useContext } from 'react'
-import { data } from '../../../data'
+import React, { useState, useContext } from "react";
+import { data } from "../../../data";
 // more components
 // fix - context api, redux (for more complex cases)
-
+const PersonContext = React.createContext();
+// iki bileşen -Provider, Consumer
 const ContextAPI = () => {
-  const [people, setPeople] = useState(data)
+  const [people, setPeople] = useState(data);
   const kaldırılmışKişi = (id) => {
     setPeople((people) => {
-      return people.filter((person) => person.id !== id)
-    })
-  }
+      return people.filter((person) => person.id !== id);
+    });
+  };
   return (
-    <section>
-      <h3>prop drilling</h3>
-      <List people={people} kaldırılmışKişi={kaldırılmışKişi} />
-    </section>
-  )
-}
+    <PersonContext.Provider value={{ kaldırılmışKişi, people }}>
+      <h3>Context Api / useContext</h3>
+      <List />
+    </PersonContext.Provider>
+  );
+};
 
-const List = ({ people, kaldırılmışKişi }) => {
+const List = () => {
+  const { people } = useContext(PersonContext);
   return (
     <>
       {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            kaldırılmışKişi={kaldırılmışKişi}
-          />
-        )
+        return <SinglePerson key={person.id} {...person} />;
       })}
     </>
-  )
-}
+  );
+};
 
-const SinglePerson = ({ id, name, kaldırılmışKişi }) => {
+const SinglePerson = ({ id, name }) => {
+  const { kaldırılmışKişi } = useContext(PersonContext);
+  //console.log(data);
   return (
-    <div className='item'>
+    <div className="item">
       <h4>{name}</h4>
       <button onClick={() => kaldırılmışKişi(id)}>kaldır</button>
     </div>
-  )
-}
+  );
+};
 
-export default ContextAPI
+export default ContextAPI;
